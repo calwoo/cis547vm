1eceaa33d8811ceeca1557c4a6e824579f0273e8
calvin.d.woo <calvin.d.woo@gmail.com>
2025-12-20 17:28:06 -0500

    complete passes for lab 2

--- lab2/src/DynamicAnalysisPass.cpp
+++ lab2/src/DynamicAnalysisPass.cpp
@@ -3,115 +3,140 @@
 
 using namespace llvm;
 
-namespace instrument {
+namespace instrument
+{
 
-const auto PASS_DESC = "Dynamic Analysis Pass";
-const auto COVERAGE_FUNCTION_NAME = "__coverage__";
-const auto BINOP_OPERANDS_FUNCTION_NAME = "__binop_op__";
+  const auto PASS_DESC = "Dynamic Analysis Pass";
+  const auto COVERAGE_FUNCTION_NAME = "__coverage__";
+  const auto BINOP_OPERANDS_FUNCTION_NAME = "__binop_op__";
 
-void instrumentCoverage(Module *M, Instruction &I, int Line, int Col);
-void instrumentBinOpOperands(Module *M, BinaryOperator *BinOp, int Line, int Col);
+  void instrumentCoverage(Module *M, Instruction &I, int Line, int Col);
+  void instrumentBinOpOperands(Module *M, BinaryOperator *BinOp, int Line, int Col);
 
-PreservedAnalyses DynamicAnalysisPass::run(Module &M, ModuleAnalysisManager &AM) {
-  outs() << "Running " << PASS_DESC << " on module " << M.getName() << "\n";
+  PreservedAnalyses DynamicAnalysisPass::run(Module &M, ModuleAnalysisManager &AM)
+  {
+    outs() << "Running " << PASS_DESC << " on module " << M.getName() << "\n";
 
-  LLVMContext &Context = M.getContext();
-  Type *VoidType = Type::getVoidTy(Context);
-  Type *Int32Type = Type::getInt32Ty(Context);
-  Type *Int8Type = Type::getInt8Ty(Context);
+    LLVMContext &Context = M.getContext();
+    Type *VoidType = Type::getVoidTy(Context);
+    Type *Int32Type = Type::getInt32Ty(Context);
+    Type *Int8Type = Type::getInt8Ty(Context);
 
-  // Declare external functions
-  M.getOrInsertFunction(COVERAGE_FUNCTION_NAME, VoidType, Int32Type, Int32Type);
-  M.getOrInsertFunction(BINOP_OPERANDS_FUNCTION_NAME,
-      VoidType,
-      Int8Type,
-      Int32Type,
-      Int32Type,
-      Int32Type,
-      Int32Type);
+    // Declare external functions
+    M.getOrInsertFunction(COVERAGE_FUNCTION_NAME, VoidType, Int32Type, Int32Type);
+    M.getOrInsertFunction(BINOP_OPERANDS_FUNCTION_NAME,
+                          VoidType,
+                          Int8Type,
+                          Int32Type,
+                          Int32Type,
+                          Int32Type,
+                          Int32Type);
 
-  for (auto &F : M) {
-    if (F.isDeclaration())
-      continue;
-
-    auto FunctionName = F.getName().str();
-    outs() << "Instrumenting function " << FunctionName << "\n";
-
-    outs() << "Instrument Instructions\n";
-
-    // Store instructions to instrument to avoid iterator invalidation
-    std::vector<std::pair<Instruction *, std::pair<int, int>>> toInstrument;
-
-    for (inst_iterator Iter = inst_begin(F), E = inst_end(F); Iter != E; ++Iter) {
-      Instruction &Inst = (*Iter);
-      llvm::DebugLoc DebugLoc = Inst.getDebugLoc();
-      if (!DebugLoc) {
-        // Skip Instruction if it doesn't have debug information.
+    for (auto &F : M)
+    {
+      if (F.isDeclaration())
         continue;
+
+      auto FunctionName = F.getName().str();
+      outs() << "Instrumenting function " << FunctionName << "\n";
+
+      outs() << "Instrument Instructions\n";
+
+      // Store instructions to instrument to avoid iterator invalidation
+      std::vector<std::pair<Instruction *, std::pair<int, int>>> toInstrument;
+
+      for (inst_iterator Iter = inst_begin(F), E = inst_end(F); Iter != E; ++Iter)
+      {
+        Instruction &Inst = (*Iter);
+        llvm::DebugLoc DebugLoc = Inst.getDebugLoc();
+        if (!DebugLoc)
+        {
+          // Skip Instruction if it doesn't have debug information.
+          continue;
+        }
+
+        int Line = DebugLoc.getLine();
+        int Col = DebugLoc.getCol();
+        toInstrument.push_back({&Inst, {Line, Col}});
       }
 
-      int Line = DebugLoc.getLine();
-      int Col = DebugLoc.getCol();
-      toInstrument.push_back({&Inst, {Line, Col}});
+      // Now instrument the collected instructions
+      for (auto &instInfo : toInstrument)
+      {
+        Instruction *Inst = instInfo.first;
+        int Line = instInfo.second.first;
+        int Col = instInfo.second.second;
+
+        instrumentCoverage(&M, *Inst, Line, Col);
+
+        /**
+         * TODO: Add code to check if the instruction is a BinaryOperator and if so,
+         * instrument the instruction as specified in the Lab document.
+         */
+        if (BinaryOperator *BinOp = dyn_cast<BinaryOperator>(Inst))
+        {
+          instrumentBinOpOperands(&M, BinOp, Line, Col);
+        }
+      }
     }
 
-    // Now instrument the collected instructions
-    for (auto &instInfo : toInstrument) {
-      Instruction *Inst = instInfo.first;
-      int Line = instInfo.second.first;
-      int Col = instInfo.second.second;
-
-      instrumentCoverage(&M, *Inst, Line, Col);
-
-      /**
-       * TODO: Add code to check if the instruction is a BinaryOperator and if so,
-       * instrument the instruction as specified in the Lab document.
-       */
-    }
+    return PreservedAnalyses::none();
   }
 
-  return PreservedAnalyses::none();
-}
+  void instrumentCoverage(Module *M, Instruction &I, int Line, int Col)
+  {
+    auto &Context = M->getContext();
+    auto *Int32Type = Type::getInt32Ty(Context);
 
-void instrumentCoverage(Module *M, Instruction &I, int Line, int Col) {
-  auto &Context = M->getContext();
-  auto *Int32Type = Type::getInt32Ty(Context);
+    auto LineVal = ConstantInt::get(Int32Type, Line);
+    auto ColVal = ConstantInt::get(Int32Type, Col);
 
-  auto LineVal = ConstantInt::get(Int32Type, Line);
-  auto ColVal = ConstantInt::get(Int32Type, Col);
+    std::vector<Value *> Args = {LineVal, ColVal};
 
-  std::vector<Value *> Args = {LineVal, ColVal};
+    auto *CoverageFunction = M->getFunction(COVERAGE_FUNCTION_NAME);
+    CallInst::Create(CoverageFunction, Args, "", &I);
+  }
 
-  auto *CoverageFunction = M->getFunction(COVERAGE_FUNCTION_NAME);
-  CallInst::Create(CoverageFunction, Args, "", &I);
-}
+  void instrumentBinOpOperands(Module *M, BinaryOperator *BinOp, int Line, int Col)
+  {
+    auto &Context = M->getContext();
+    auto *Int32Type = Type::getInt32Ty(Context);
+    auto *CharType = Type::getInt8Ty(Context);
 
-void instrumentBinOpOperands(Module *M, BinaryOperator *BinOp, int Line, int Col) {
-  auto &Context = M->getContext();
-  auto *Int32Type = Type::getInt32Ty(Context);
-  auto *CharType = Type::getInt8Ty(Context);
+    /**
+     * TODO: Add code to instrument the BinaryOperator to print
+     * its location, operation type and the runtime values of its
+     * operands.
+     */
+    char binop_symbol = getBinOpSymbol(BinOp->getOpcode());
+    auto LineVal = ConstantInt::get(Int32Type, Line);
+    auto ColVal = ConstantInt::get(Int32Type, Col);
+    auto OpCharVal = ConstantInt::get(CharType, binop_symbol);
 
-  /**
-   * TODO: Add code to instrument the BinaryOperator to print
-   * its location, operation type and the runtime values of its
-   * operands.
-   */
-}
+    std::vector<Value *> Args = {OpCharVal, LineVal, ColVal, BinOp->getOperand(0), BinOp->getOperand(1)};
 
-// Pass registration for the new pass manager
-extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo llvmGetPassPluginInfo() {
-  return {LLVM_PLUGIN_API_VERSION, "DynamicAnalysisPass", "1.0.0", [](PassBuilder &PB) {
-            PB.registerPipelineParsingCallback(
-                [](StringRef Name,
-                    ModulePassManager &MPM,
-                    ArrayRef<PassBuilder::PipelineElement>) {
-                  if (Name == "DynamicAnalysisPass") {
-                    MPM.addPass(DynamicAnalysisPass());
-                    return true;
-                  }
-                  return false;
-                });
-          }};
-}
+    auto *CoverageFunction = M->getFunction(BINOP_OPERANDS_FUNCTION_NAME);
+    CallInst::Create(CoverageFunction, Args, "", BinOp);
+  }
 
-}  // namespace instrument
+  // Pass registration for the new pass manager
+  extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo llvmGetPassPluginInfo()
+  {
+    return {LLVM_PLUGIN_API_VERSION, "DynamicAnalysisPass", "1.0.0", [](PassBuilder &PB)
+            {
+              PB.registerPipelineParsingCallback(
+                  [](StringRef Name,
+                     ModulePassManager &MPM,
+                     ArrayRef<PassBuilder::PipelineElement>)
+                  {
+                    if (Name == "DynamicAnalysisPass")
+                    {
+                      MPM.addPass(DynamicAnalysisPass());
+                      return true;
+                    }
+                    return false;
+                  });
+            }};
+  }
+
+} // namespace instrument
--- lab2/src/StaticAnalysisPass.cpp
+++ lab2/src/StaticAnalysisPass.cpp
@@ -4,57 +4,73 @@
 
 using namespace llvm;
 
-namespace instrument {
+namespace instrument
+{
 
-const auto PASS_DESC = "Static Analysis Pass";
+  const auto PASS_DESC = "Static Analysis Pass";
 
-PreservedAnalyses StaticAnalysisPass::run(Module &M, ModuleAnalysisManager &AM) {
-  outs() << "Running " << PASS_DESC << " on module " << M.getName() << "\n";
+  PreservedAnalyses StaticAnalysisPass::run(Module &M, ModuleAnalysisManager &AM)
+  {
+    outs() << "Running " << PASS_DESC << " on module " << M.getName() << "\n";
 
-  for (auto &F : M) {
-    if (F.isDeclaration())
-      continue;
-
-    auto FunctionName = F.getName().str();
-    outs() << "Running " << PASS_DESC << " on function " << FunctionName << "\n";
-
-    outs() << "Locating Instructions\n";
-    for (inst_iterator Iter = inst_begin(F), E = inst_end(F); Iter != E; ++Iter) {
-      Instruction &Inst = (*Iter);
-      llvm::DebugLoc DebugLoc = Inst.getDebugLoc();
-      if (!DebugLoc) {
-        // Skip Instruction if it doesn't have debug information.
+    for (auto &F : M)
+    {
+      if (F.isDeclaration())
         continue;
+
+      auto FunctionName = F.getName().str();
+      outs() << "Running " << PASS_DESC << " on function " << FunctionName << "\n";
+
+      outs() << "Locating Instructions\n";
+      for (inst_iterator Iter = inst_begin(F), E = inst_end(F); Iter != E; ++Iter)
+      {
+        Instruction &Inst = (*Iter);
+        llvm::DebugLoc DebugLoc = Inst.getDebugLoc();
+        if (!DebugLoc)
+        {
+          // Skip Instruction if it doesn't have debug information.
+          continue;
+        }
+
+        int Line = DebugLoc.getLine();
+        int Col = DebugLoc.getCol();
+        outs() << Line << ", " << Col << "\n";
+
+        /**
+         * TODO: Add code to check if the instruction is a BinaryOperator and if
+         * so, print the information about its location and operands as specified
+         * in the Lab document.
+         */
+        if (BinaryOperator *BinOp = dyn_cast<BinaryOperator>(&Inst))
+        {
+          char binop_symbol = getBinOpSymbol(BinOp->getOpcode());
+          std::string op1 = variable(BinOp->getOperand(0));
+          std::string op2 = variable(BinOp->getOperand(1));
+          outs() << getBinOpName(binop_symbol) << " on Line " << Line << ", Column " << Col << " with first operand " << op1 << " and second operand " << op2 << "\n";
+        }
       }
-
-      int Line = DebugLoc.getLine();
-      int Col = DebugLoc.getCol();
-      outs() << Line << ", " << Col << "\n";
-
-      /**
-       * TODO: Add code to check if the instruction is a BinaryOperator and if
-       * so, print the information about its location and operands as specified
-       * in the Lab document.
-       */
     }
+    return PreservedAnalyses::all();
   }
-  return PreservedAnalyses::all();
-}
 
-// Pass registration for the new pass manager
-extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo llvmGetPassPluginInfo() {
-  return {LLVM_PLUGIN_API_VERSION, "StaticAnalysisPass", "1.0.0", [](PassBuilder &PB) {
-            PB.registerPipelineParsingCallback(
-                [](StringRef Name,
-                    ModulePassManager &MPM,
-                    ArrayRef<PassBuilder::PipelineElement>) {
-                  if (Name == "StaticAnalysisPass") {
-                    MPM.addPass(StaticAnalysisPass());
-                    return true;
-                  }
-                  return false;
-                });
-          }};
-}
+  // Pass registration for the new pass manager
+  extern "C" LLVM_ATTRIBUTE_WEAK PassPluginLibraryInfo llvmGetPassPluginInfo()
+  {
+    return {LLVM_PLUGIN_API_VERSION, "StaticAnalysisPass", "1.0.0", [](PassBuilder &PB)
+            {
+              PB.registerPipelineParsingCallback(
+                  [](StringRef Name,
+                     ModulePassManager &MPM,
+                     ArrayRef<PassBuilder::PipelineElement>)
+                  {
+                    if (Name == "StaticAnalysisPass")
+                    {
+                      MPM.addPass(StaticAnalysisPass());
+                      return true;
+                    }
+                    return false;
+                  });
+            }};
+  }
 
-}  // namespace instrument
+} // namespace instrument
